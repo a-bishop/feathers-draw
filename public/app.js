fe2e6095020a6d36a175f0aba323ec9650d0adb6
a-bishop <andrew.bishop53@gmail.com>
2019-12-01 00:04:33 -0800

    Remove uuidv4 library

--- public/app.js
+++ public/app.js
@@ -20,6 +20,14 @@ let firstPointRecorded = false;
 let uuid = null;
 let localLine = { x: [], y: [] };
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 ['click', 'touchstart'].forEach(eventType =>
   clearButton.addEventListener(eventType, async e => {
     e.preventDefault();
